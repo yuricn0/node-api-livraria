f8f769c471f83e0f3b5196d983c7bd5b026e9895
Yuri Nunes <yuri.cruz10@hotmail.com>
2025-04-09 14:28:07 -0300

    Adicionando express

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-      "/": "Express API",
-      "/livros": "Livros",
-      "/autores": "Autores"
-};
-
-const server = http.createServer((req, res) => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
       console.log("Server running at http://localhost:3000/");
 });
-
-
-
